6991d9135fee4ebe5ca77bbe0261e75d54e73242
Rafael Urbano <rafezin2121@gmail.com>
2024-09-23 08:49:18 -0300

    configuracao banco de dados

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,5 +1,26 @@
-namespace MinimalApi.Infraestruta.Db;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
 
-public class DbContexto {
-    
+namespace MinimalApi.Infraestrura.Db;
+
+public class DbContexto : DbContext {
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DbContexto(IConfiguration configuracaoAppSettings) {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){   
+        if(!optionsBuilder.IsConfigured){
+
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if(!string.IsNullOrEmpty(stringConexao)){
+                optionsBuilder.UseMySql(
+                    stringConexao, 
+                    ServerVersion.AutoDetect(stringConexao)
+                );
+            }
+        }
+        
+    }
 }
